a63e27ac4339f5949172a453ac05df5e0a1d30a3
Kamil Barwiński <kamilb.kontakt@gmail.com>
2021-12-11 20:07:39 +0100

    summary

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ppk_main
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Helloss World!");
+        }
+    }
+}
