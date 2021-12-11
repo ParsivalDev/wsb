a3b28980be5268e17858a581878cd536415a3467
Kamil Barwiński <kamilb.kontakt@gmail.com>
2021-12-11 20:47:44 +0100

    ready to check

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ppk_main
             Console.WriteLine("Wpisz liczbe nr 2");
             int liczba_d = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Wprowadz '+' dla dodawania, '-' dla odejmowani lub '*' dla mnożenia");
+            Console.WriteLine("Wprowadz '+' dla dodawania, '-' dla odejmowani, '*' dla mnożenia lub '/' dla dzielenia");
             char znak = Console.ReadLine()[0];
 
 
@@ -31,6 +31,16 @@ namespace ppk_main
 
                 case '*':
 
+                    Console.WriteLine(liczba_j * liczba_d);
+                    break;
+
+                case '/':
+
+                    if(liczba_j == 0 || liczba_d == 0)
+                    {
+                        Console.WriteLine("Nie można dzielić przez 0, sprobuj ponownie!");
+                    }
+                    else
                     Console.WriteLine(liczba_j * liczba_d);
                     break;
 
