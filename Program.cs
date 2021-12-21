66ce25b78393e54efbf598d7088bc0f710e316f2
Kamil Barwiński <kamilb.kontakt@gmail.com>
2021-12-21 18:53:01 +0100

    FULL SILNIE

--- Program.cs
+++ Program.cs
@@ -6,12 +6,33 @@ namespace ppk_main
     {
         public static void Main(string[] args)
         {
-            ulong silnia = 1;
+            int silnia1 = 1;
+            Console.WriteLine("Silnia z liczby 9: ");
+            for (int i = 1; i <= 9; i++)
+            {
+
+                silnia1 *= i; // silnia = silnia * i
+                Console.WriteLine(silnia1);
+
+            }
+            double silnia3 = 1;
+            Console.WriteLine("Silnia z liczby 20: ");
+            for (double i = 1; i <= 20; i++)
+            {
+
+                silnia3 *= i; // silnia = silnia * i
+                Console.WriteLine(silnia3);
+
+            }
+
+
+            ulong silnia2 = 1;
+            Console.WriteLine("Silnia z liczby 243: ");
             for (ulong i = 1; i <= 243; i++)
             {
 
-                silnia *= i; // silnia = silnia * i
-                Console.WriteLine(silnia);
+                silnia2 *= i; // silnia = silnia * i
+                Console.WriteLine(silnia2);
 
             }
         }
